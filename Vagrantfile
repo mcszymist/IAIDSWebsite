1a49f0e41ada0b8c8e31c638ab788d65dd96bc90
FromAlaska <thealaskanfrontier@gmail.com>
2019-01-23 17:45:49 -0900

    added important files

---
+++ Vagrantfile
@@ -0,0 +1,32 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  # Base box to use
+  config.vm.box = "bento/ubuntu-18.04"
+
+  config.vm.provider "virtualbox" do |v|
+    v.gui = true
+    v.name = "IAIDSVM"
+    v.memory = 2048
+    v.cpus = 1
+    v.customize ["modifyvm",:id,"--natdnshostresolver1","on"]
+  end
+
+  # Forwarded ports
+  config.vm.network "forwarded_port", guest: 8000, host: 8111, host_ip:"0.0.0.0"
+  config.vm.network "forwarded_port", guest: 80, host: 8112, host_ip:"127.0.0.1"
+  config.vm.network "forwarded_port", guest: 443, host: 8113, host_ip:"127.0.0.1"
+  config.vm.network "forwarded_port", guest: 3306, host: 8114, host_ip:"127.0.0.1"
+
+  # Enable provisioning with a shell script.
+  config.vm.provision :shell, :path => "provision.sh", :args => "true"
+
+  # k-12 platform project files
+  config.vm.synced_folder ".", "/home/vagrant/uafHvZSite"
+  # Media directory, with write mode given to the vagrant account/group.
+  #config.vm.synced_folder "media", "/srv/platform/media", create: true,
+    #mount_options: ["dmode=775,fmode=775"]
+end
---
+++ provision.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+
+# update the vm
+sudo apt-get update
+
+# For provision.sh
+sudo apt-get install dos2unix
+
+# Install pip3
+sudo apt-get install python3-pip -y
+
+# Install Django
+pip3 install django
+
+# Install mysql server
+sudo apt-get install mysql-server -y
+
+# Creates a database
+sudo mysql -e "CREATE DATABASE IF NOT EXISTS IAIDSSQL"
+sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'vagrant'@'%' IDENTIFIED BY 'Project1!';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'vagrant'@'10.0.2.15' IDENTIFIED BY 'Project1!';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'Project1!';"
+sudo mysql -e "FLUSH PRIVILEGES;"
+
+# Add extra mysql client stuff
+sudo apt-get install libmysqlclient-dev -y
+
+# Instal dependencies for the project
+pip3 install -r /home/vagrant/IAIDSWebsite/requirements.txt
+
+# got to the correct directory
+cd IAIDSWebsite/IAIDSWebsite/
+
+# migrate data to mysql server
+python3 manage.py migrate
+
+# Add firewall rules for django
+sudo ufw allow 8000
+
+# Logs in as root
+sudo su
+
+# Adds mysql configuration
+printf "[mysqld]\nbind-address = 0.0.0.0\n\n" >> /etc/mysql/my.cnf
+
+# Restart after applying mysql settings
+sudo service mysql restart
+
+# Exit root privileges
+exit;
