2406c06d4831c699a5c7c74b2cb8ee98ba621e62
Fromlaska <jasamson@alaska.edu>
2019-01-24 19:11:33 -0900

    added files and template

--- IAIDSWebsite/home/views.py
+++ IAIDSWebsite/home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/currentgame/')
+    else:
+        return render(request, 'home_template.html')
