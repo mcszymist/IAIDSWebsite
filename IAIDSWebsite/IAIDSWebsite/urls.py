ccdffed7ce7b04ce5abfdfc0b35dba5647f6e1d1
Acheeseye <hsi_jason@hotmail.com>
2019-01-26 17:06:15 -0900

    [#1,2][settings.py urls.py] Readded files deleted from merge

---
+++ IAIDSWebsite/IAIDSWebsite/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import TemplateView
+
+urlpatterns = [
+    path('', include('home.urls')),
+    path('createAccount/', include('createAccount.urls')),
+]
