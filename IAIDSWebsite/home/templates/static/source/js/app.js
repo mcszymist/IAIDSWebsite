6a2a5aa2d8f4885d8ba7e2c677fa0c8db30d24ea
Acheeseye <hsi_jason@hotmail.com>
2019-01-26 16:16:47 -0900

    [#1,2][new directory] Update static directory (js & css)

---
+++ IAIDSWebsite/home/templates/static/source/js/app.js
@@ -0,0 +1,7 @@
+$(document).foundation();
+
+$('.top-bar').on('sticky.zf.stuckto:top', function () {
+    $(this).addClass('shrink');
+}).on('sticky.zf.unstuckfrom:top', function () {
+    $(this).removeClass('shrink');
+})
