dc641aea06e8d427e51346c5cfb4e86a299ff0c2
Acheeseye <hsi_jason@hotmail.com>
2019-01-26 16:24:44 -0900

    '

--- IAIDSWebsite/home/static/source/js/app.js
+++ IAIDSWebsite/home/static/source/js/app.js
@@ -1,1 +1,7 @@
-$(document).foundation()
+$(document).foundation();
+
+$('.title-bar').on('sticky.zf.stuckto:top', function () {
+    $(this).addClass('shrink');
+}).on('sticky.zf.unstuckfrom:top', function () {
+    $(this).removeClass('shrink');
+})
