85942faf312e7efb14be5dff22a96088419f783f
FromAlaska <thealaskanfrontier@gmail.com>
2019-01-31 18:29:09 -0900

    added sign up page, created a page for sign up. Sign in/ sign up now works. Accounts are added to the database

--- IAIDSWebsite/createAccount/urls.py
+++ IAIDSWebsite/createAccount/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.createAccount, name='createAccount'),
+    path('', views.signup, name='signup'),
 ]
--- IAIDSWebsite/createAccount/views.py
+++ IAIDSWebsite/createAccount/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+# # Create your views here.
 
-def createAccount(request):
-    return render(request, 'createAccount/createAccount.html')
+# def createAccount(request):
+#     return render(request, 'createAccount/createAccount.html')
+
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'createAccount/signup.html', {'form': form})
