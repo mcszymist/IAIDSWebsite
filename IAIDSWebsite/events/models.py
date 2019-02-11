d47fe546f7054257f153cb98efcbf6e11bc32569
zymist <mcszymist7@gmail.com>
2019-02-10 18:00:37 -0900

    [#][*multiply*] Updated dependecy also updated the calender to have a max column size

--- IAIDSWebsite/events/models.py
+++ IAIDSWebsite/events/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    UniqueCode = models.CharField(max_length=100)
+    Title = models.CharField(max_length=100)
+    Description = models.CharField(max_length=300)
+    Organization = models.CharField(max_length=100)
+    typeOfVol = models.CharField(max_length=300)
+    location = models.CharField(max_length=100)
+    startDate = models.DateTimeField('date published')
+    endDate = models.DateTimeField('date published')
+    
