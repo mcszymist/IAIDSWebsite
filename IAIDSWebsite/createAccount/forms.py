d188a89e0abfe874e457b6f0eb1f79c23718f1d2
Darkpony <dponyg@gmail.com>
2019-02-11 13:28:45 -0800

    #ISSUE 31 Added some css to the signup form

--- IAIDSWebsite/createAccount/forms.py
+++ IAIDSWebsite/createAccount/forms.py
@@ -6,9 +6,9 @@ from django import forms
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Required.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Required.')
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=False)
+    email = forms.EmailField(max_length=254, required = True)
 
     
     class Meta:
