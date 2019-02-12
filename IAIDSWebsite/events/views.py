378a7c02d6c6dd2cfd4259c1e0ba009ff1015b9d
zymist <mcszymist7@gmail.com>
2019-02-11 15:35:06 -0900

    [#7][datatables] Added datatables to have faster development with tables.

--- IAIDSWebsite/events/views.py
+++ IAIDSWebsite/events/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def events(request):
     return render(request, 'events/events.html')
+
+def eventsFilter(request):
+    return render(request, 'events/eventsFilter.html')
