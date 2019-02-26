17d00d1e8c66828e67851375900ea0a53e142386
zymist <mcszymist7@gmail.com>
2019-02-26 01:49:23 -0900

    [#43][eventEdit] Created Module for events and added some css to eventEdit page

--- IAIDSWebsite/eventEdit/models.py
+++ IAIDSWebsite/eventEdit/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    volMax = models.IntegerField()
+    volCurrent = models.IntegerField()
+    location = models.CharField(max_length=100)
+    startdate = models.DateTimeField()
+    enddate = models.DateTimeField()
+    uid = models.UUIDField(primary_key=True)
+    
