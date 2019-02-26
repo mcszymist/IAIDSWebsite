c3428217b62369a52277ced9a67513e3d0d5155e
Daniel Lind <dlind777@gmail.com>
2019-02-25 21:54:11 -0900

    Pushing changes done to profileEdit page, still doesn't update the user model from what I can tell

--- IAIDSWebsite/profileEditor/views.py
+++ IAIDSWebsite/profileEditor/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.utils.encoding import smart_str
 from django.http import HttpResponse
 from IAIDSWebsite import settings
+from createAccount import models
 
 from .forms import profileEditForm
 from django.contrib.auth.models import User
@@ -18,12 +19,18 @@ def profileImage(request,file_name):
     return response
 
 def edit(request):
+    curr_email = request.user.email
+    curr_user = models.MyUser.objects.get(email=curr_email)
+
+    form = profileEditForm(request.POST, request.FILES,
+                                 instance=curr_user)
     if request.method == 'POST':
-        form = profileEditForm(request.POST, instance=self)
+
+        #form = profileEditForm(instance=current_user)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect('/thanks/')
+            #return HttpResponseRedirect('profileManage')
     else:
-        form = profileEditForm()
+        form = profileEditForm(request.POST)
     return render(request, 'profileEditor/profileEdit.html', {'form': form})
 
