9f29bcc5cb3432e483edfc708fd4f63d25494948
Acheeseye <hsi_jason@hotmail.com>
2019-04-03 01:02:06 -0800

    [misc][py] upcomingEvents variable added to home.html render

--- IAIDSWebsite/home/views.py
+++ IAIDSWebsite/home/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.db.models import Q
+
+from orgAdminPanel.models import Event
+
 from allauth.account.views import LoginView
 from allauth.account.forms import LoginForm
+
 from .forms import MyCustomLoginForm
+
+from datetime import datetime, date, timedelta
 
 # Create your views here.
 
 
 def index(request):
-    # if request.user.is_authenticated:
-    #     return HttpResponseRedirect('')
-    # else:
-    return render(request, 'home.html')
+    startdateBegin = datetime.today()
+    startdateEnd = startdateBegin + timedelta(days=7)
+    starttimeBegin = datetime.time(datetime.now())
 
-
-  
+    testArray = [startdateBegin, startdateEnd]
+    upcomingEvents = Event.objects.all().order_by(
+        'startdate', 'starttime', 'name').filter(startdate__range=[startdateBegin, startdateEnd],
+                                                 starttime__gte=starttimeBegin)
+    return render(request, 'home.html',
+                  {'testArray': testArray,
+                   'upcomingEvents': upcomingEvents})
