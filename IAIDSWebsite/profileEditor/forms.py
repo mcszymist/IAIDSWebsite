822b747b42af69dbb77deff48a08689692d7792f
Daniel Lind <dlind777@gmail.com>
2019-04-30 13:25:40 -0800

    Improved css on profile edit page.

--- IAIDSWebsite/profileEditor/forms.py
+++ IAIDSWebsite/profileEditor/forms.py
@@ -1,5 +1,5 @@
 from django.core.exceptions import NON_FIELD_ERRORS
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea
 from createAccount import models
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserChangeForm
@@ -16,3 +16,6 @@ class profileEditForm(UserChangeForm):
                   'description',
                   'profile_pic'
                   ]
+        widgets = {
+            'description': Textarea(attrs={'cols': 20, 'rows': 6})
+        }
